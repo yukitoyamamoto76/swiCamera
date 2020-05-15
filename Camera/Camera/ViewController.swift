09181d075163179b6f5618cadffba53bdb2137e3
yukitoyamamoto76 <56571664+yukitoyamamoto76@users.noreply.github.com>
2020-05-15 21:23:35 +0900

    initial comit
    最初のコミットです

---
+++ Camera/Camera/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 山本優気人 on 2019/08/21.
+//  Copyright © 2019 YukitoYamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        
+        let camera = UIImagePickerController.SourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
